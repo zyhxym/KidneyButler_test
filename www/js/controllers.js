dcd5f40a0af65369c94f0fd922e4d227de219502
BME319 <zjubme319@163.com>
2017-05-25 22:44:08 +0800

    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1065,29 +1065,27 @@ angular.module('kidney.controllers', ['ionic','kidney.services','ngResource','io
                                                           {
                                                             User.setMessageOpenId({type:2,userId:patientId,openId:Storage.get('messageopenid')}).then(function(res){
                                                                 console.log("setopenid");
-                                                                if (angular.isDefined($scope.User.weight) == true)
-                                                                {
-                                                                  VitalSign.insertVitalSign({patientId:patientId, type: "Weight",code: "Weight_1", date:now.substr(0,10),datatime:now,datavalue:$scope.User.weight,unit:"kg"}).then(function(data){
-                                                                      // $scope.User.weight = data.results;
-                                                                      console.log($scope.User);
-                                                                      
-                                                                      $state.go('signin');
-                                                                  },function(err){
-                                                                      $ionicLoading.show({
-                                                                          template: '注册失败',
-                                                                          duration:1000
-                                                                      });
-                                                                      $state.go('signin');
-                                                                      console.log("插入体重"+err);
-                                                                  });
-                                                                }
-                                                                else{
-                                                                  $state.go('signin');
-                                                                }
                                                             },function(){
                                                                 console.log("连接超时！");
                                                             })
-                                                            
+                                                            if (angular.isDefined($scope.User.weight) == true)
+                                                            {
+                                                              VitalSign.insertVitalSign({patientId:patientId, type: "Weight",code: "Weight_1", date:now.substr(0,10),datatime:now,datavalue:$scope.User.weight,unit:"kg"}).then(function(data){
+                                                                  // $scope.User.weight = data.results;
+                                                                  console.log($scope.User);
+                                                                  
+                                                                  $state.go('signin');
+                                                              },function(err){
+                                                                  $ionicLoading.show({
+                                                                      template: '注册失败',
+                                                                      duration:1000
+                                                                  });
+                                                                  console.log("插入体重"+err);
+                                                              });
+                                                            }
+                                                            else{
+                                                              $state.go('signin');
+                                                            }
                                                           }
                                                       },function(){
                                                           $scope.logStatus = "连接超时！";
